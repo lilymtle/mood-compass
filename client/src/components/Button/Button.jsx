91203d07ebdb3a00c9db658346cc5aa69baa5375
Lily Le <lmlt8180@gmail.com>
2024-09-11 16:51:39 -0400

    create button component (.jsx and .scss)
    - add props

---
+++ client/src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+export function Button({ className, type, text, onClick }) {
+    return (
+        <button
+        className={className}
+        type={type}
+        onClick={onClick}>
+            {text}
+        </button>
+    )
+};
