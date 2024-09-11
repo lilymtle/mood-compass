88484cbc620fe1e508dead3d96815266c5407190
Lily Le <lmlt8180@gmail.com>
2024-09-11 16:54:16 -0400

    update class names

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -7,13 +7,13 @@ export function NavBar() {
                 <li className="nav-bar__item">
                     <a className="nav-bar__link" href="/">Home</a>
                 </li>
-                <li className="nav-bar__list">
+                <li className="nav-bar__item">
                     <a className="nav-bar__link" href="/">About</a>
                 </li>
-                <li className="nav-bar__list">
+                <li className="nav-bar__item">
                     <a className="nav-bar__link" href="/">Moods</a>
                 </li>
-                <li className="nav-bar__list">
+                <li className="nav-bar__item">
                     <a className="nav-bar__link" href="/">Learn</a>
                 </li>
             </ul>
