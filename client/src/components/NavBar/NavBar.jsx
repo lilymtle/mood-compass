01df60b6ff746d836b3294e3e4b051bd4a7532cc
Lily Le <lmlt8180@gmail.com>
2024-09-11 16:52:15 -0400

    create navbar component (.jsx and .scss)
    - add navigation links

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,22 @@
+import "./NavBar.scss";
+
+export function NavBar() {
+    return (
+        <nav className="nav-bar">
+            <ul className="nav-bar__list">
+                <li className="nav-bar__item">
+                    <a className="nav-bar__link" href="/">Home</a>
+                </li>
+                <li className="nav-bar__list">
+                    <a className="nav-bar__link" href="/">About</a>
+                </li>
+                <li className="nav-bar__list">
+                    <a className="nav-bar__link" href="/">Moods</a>
+                </li>
+                <li className="nav-bar__list">
+                    <a className="nav-bar__link" href="/">Learn</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
