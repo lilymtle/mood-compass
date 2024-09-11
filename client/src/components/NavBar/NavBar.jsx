3ee067cafc5aff4acbf224460ff45ccdfcabaf16
Lily Le <lmlt8180@gmail.com>
2024-09-11 18:05:09 -0400

    add div container and style dropdown menu

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -4,32 +4,34 @@ import "./NavBar.scss";
 export function NavBar() {
     return (
         <nav className="nav-bar">
-            <ul className="nav-bar__list">
-                <li className="nav-bar__item">
-                    <a className="nav-bar__link" href="/">Home</a>
-                </li>
-                <li className="nav-bar__item">
-                    <a className="nav-bar__link" href="/">About</a>
-                </li>
-                <li className="nav-bar__item">
-                    <a className="nav-bar__link" href="/">Moods</a>
-                </li>
-                <li className="nav-bar__item">
-                    <div className="dropdown">
-                        <Button className="dropdown__btn" text="Learn" />
-                        <div className="dropdown__content">
-                            <ul className="dropdown__list">
-                                <li className="dropdown__item">
-                                    <a className="dropdown__link" href="/">Educational Resources</a>
-                                </li>
-                                <li className="dropdown__item">
-                                    <a className="dropdown__link" href="/">Coping Strategies</a>
-                                </li>
-                            </ul>
+            <div className="nav-bar__container">
+                <ul className="nav-bar__list">
+                    <li className="nav-bar__item">
+                        <a className="nav-bar__link" href="/">Home</a>
+                    </li>
+                    <li className="nav-bar__item">
+                        <a className="nav-bar__link" href="/">About</a>
+                    </li>
+                    <li className="nav-bar__item">
+                        <a className="nav-bar__link" href="/">Moods</a>
+                    </li>
+                    <li className="nav-bar__item">
+                        <div className="dropdown">
+                            <Button className="dropdown__btn" text="Learn" />
+                            <div className="dropdown__content">
+                                <ul className="dropdown__list">
+                                    <li className="dropdown__item">
+                                        <a className="dropdown__link" href="/">Educational Resources</a>
+                                    </li>
+                                    <li className="dropdown__item">
+                                        <a className="dropdown__link" href="/">Coping Strategies</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                </li>
-            </ul>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
