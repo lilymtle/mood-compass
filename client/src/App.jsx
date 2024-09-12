518a36dec1ce6852ccd31858e1867bf4015e12d2
Lily Le <lmlt8180@gmail.com>
2024-09-12 12:19:48 -0400

    import login and not found page, add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import { AboutPage } from "./pages/AboutPage/AboutPage.jsx";
 import { MoodsPage } from "./pages/MoodsPage/MoodsPage.jsx";
 import { EducationalResourcesPage } from "./pages/EducationalResourcesPage/EducationalResourcesPage.jsx";
 import { CopingStrategiesPage } from "./pages/CopingStrategiesPage/CopingStrategiesPage.jsx";
+import { LoginPage } from "./pages/LoginPage/LoginPage.jsx";
+import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage.jsx";
 
 function App() {
 
@@ -24,6 +26,8 @@ function App() {
         <Route path="/moods-list" element={ <MoodsPage /> } />
         <Route path="/educational-resources" element={ <EducationalResourcesPage /> } />
         <Route path="/coping-strategies" element={ <CopingStrategiesPage /> } />
+        <Route path="/login" element={ <LoginPage /> } />
+        <Route path="*" element={ <NotFoundPage /> } />
       </Routes>
 
       <Footer />
