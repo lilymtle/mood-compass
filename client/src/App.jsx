258c3db99e24abd2bb4d49644543b8f45f8b5f4e
Lily Le <lmlt8180@gmail.com>
2024-09-11 21:44:05 -0400

    import footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import "./styles/partials/_global.scss";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }
