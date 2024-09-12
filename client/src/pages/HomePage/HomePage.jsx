1ea25716f15de24cca3df2304dd56dbef59ed629
Lily Le <lmlt8180@gmail.com>
2024-09-12 14:56:19 -0400

    add welcome message and style

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -5,7 +5,18 @@ export function HomePage() {
         <main>
             <section className="home__container">
                 <div className="home__welcome">
-                    Hi.
+                    <h1 className="home__welcome-header">
+                        Welcome!
+                    </h1>
+                    <p className="home__welcome-message">
+                        Hi, there! I am so glad you are here. MoodCompass is a capstone project
+                        for BrainStation's Software Engineering program, and it is a budding
+                        web app that strives to create positive change.
+                    </p>
+                </div>
+
+                <div className="home__">
+                    <h2 className="home__">e</h2>
                 </div>
             </section>
         </main>
