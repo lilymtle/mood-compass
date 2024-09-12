529b47fade332646117e010f7bda45e52f82cb0b
Lily Le <lmlt8180@gmail.com>
2024-09-11 22:17:32 -0400

    create and style footer component

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,10 +4,27 @@ export function Footer() {
     return (
         <footer className="footer">
             <section className="footer__left">
-                
+                <div className="footer__left-container">
+                    <p className="footer__text">
+                        Connect:
+                    </p>
+                </div>
+                <div className="footer__left-container">
+                    <img
+                    className="footer__social-icon"
+                    src="/src/assets/logos/linkedin-logo.png"
+                    alt="linkedin icon" />
+
+                    <img 
+                    className="footer__social-icon"
+                    src="/src/assets/logos/github-logo.svg"
+                    alt="github icon" />
+                </div>
             </section>
             <section className="footer__right">
-
+                <p className="footer__text">
+                © 2024, MoodCompass
+                </p>
             </section>
         </footer>
     )
