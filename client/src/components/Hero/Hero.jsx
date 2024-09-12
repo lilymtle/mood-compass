1e5c80e9997ae9e56153abd2d8beb24e6d5de3e6
Lily Le <lmlt8180@gmail.com>
2024-09-12 11:24:09 -0400

    add span tag and update styling

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -11,9 +11,9 @@ export function Hero() {
                     alt="close up of a bouqet of dried white flowers" />
                 </div>
 
-                <div className="hero__header">
-                    <p className="hero__header-text">
-                        Navigate your mood through education.
+                <div className="hero__text-container">
+                    <p className="hero__text">
+                        Navigate your mood through <span className="hero__text-important">education</span>.
                     </p>
                 </div>
             </div>
