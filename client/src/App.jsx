d31e1501322879ce817cd3ddf874dbaff945c7ef
Lily Le <lmlt8180@gmail.com>
2024-09-12 12:00:02 -0400

    import pages and update routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,31 @@ import { Header } from "./components/Header/Header";
 import { Hero } from "./components/Hero/Hero";
 import "./styles/partials/_global.scss";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { HomePage } from "./pages/HomePage/HomePage.jsx";
+import { AboutPage } from "./pages/AboutPage/AboutPage.jsx";
+import { MoodsPage } from "./pages/MoodsPage/MoodsPage.jsx";
+import { EducationalResourcesPage } from "./pages/EducationalResourcesPage/EducationalResourcesPage.jsx";
+import { CopingStrategiesPage } from "./pages/CopingStrategiesPage/CopingStrategiesPage.jsx";
+
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <Hero />
+
+      <Routes>
+        <Route path="/" element={ <HomePage /> } />
+        <Route path="/about" element={ <AboutPage /> } />
+        <Route path="/moods-list" element={ <MoodsPage /> } />
+        <Route path="/educational-resources" element={ <EducationalResourcesPage /> } />
+        <Route path="/coping-strategies" element={ <CopingStrategiesPage /> } />
+      </Routes>
+
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
