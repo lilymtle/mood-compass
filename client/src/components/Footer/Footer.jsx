d32c11674e9900b387c476199e18c3b44ba4fc9f
Lily Le <lmlt8180@gmail.com>
2024-09-11 21:43:57 -0400

    create footer component and add boilerplate code

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.scss";
+
+export function Footer() {
+    return (
+        <footer className="footer">
+            <section className="footer__left">
+                
+            </section>
+            <section className="footer__right">
+
+            </section>
+        </footer>
+    )
+}
