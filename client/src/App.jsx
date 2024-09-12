c154771d8453e95485340dea03b104b5c8094589
Lily Le <lmlt8180@gmail.com>
2024-09-12 08:59:38 -0400

    import hero component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
+import { Hero } from "./components/Hero/Hero";
 import "./styles/partials/_global.scss";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+      <Hero />
       <Footer />
     </>
   )
