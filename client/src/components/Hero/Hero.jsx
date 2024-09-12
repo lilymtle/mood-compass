255a21e995f84cff077b936871c4c06816df0fda
Lily Le <lmlt8180@gmail.com>
2024-09-12 11:07:34 -0400

    add images and style hero

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,13 +1,16 @@
 import "./Hero.scss";
 
 export function Hero() {
-    return (
-        <>
+    return (  
         <section className="hero">
             <div className="hero__container">
-
+                <div className="hero__overlay">
+                    <img 
+                    className="hero__overlay-img"
+                    src="/src/assets/images/hero-img.jpg"
+                    alt="close up of a bouqet of dried white flowers" />
+                </div>
             </div>
         </section>
-        </>
     )
 }
