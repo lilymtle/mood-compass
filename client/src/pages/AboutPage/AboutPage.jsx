5777c8f8854191f4221bc26f1001d793638dfc55
Lily Le <lmlt8180@gmail.com>
2024-09-12 20:16:44 -0400

    create sections and containers, add styling

--- client/src/pages/AboutPage/AboutPage.jsx
+++ client/src/pages/AboutPage/AboutPage.jsx
@@ -2,8 +2,24 @@ import "./AboutPage.scss";
 
 export function AboutPage() {
     return (
-        <>
-            About page holder
-        </>
+        <main>
+            <section className="about__mood-compass">
+                <div className="about__mood-compass-container">
+                    f
+                </div>
+            </section>
+
+            <section className="about__me">
+                <div className="about__me-container">
+                    Hi
+                </div>
+            </section>
+
+            <section className="about__values">
+                <div className="about__values-container">
+                    list
+                </div>
+            </section>
+        </main>
     )
 }
