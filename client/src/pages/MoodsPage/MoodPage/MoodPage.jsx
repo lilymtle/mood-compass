cbf9590da1ed5c932fe0e83565fadb2c52936e84
Lily Le <lmlt8180@gmail.com>
2024-09-15 10:20:07 -0400

    create mood page and stylesheet

---
+++ client/src/pages/MoodsPage/MoodPage/MoodPage.jsx
@@ -0,0 +1,12 @@
+/* this page is for a single mood and provides more 
+detailed information on it */
+
+import "./MoodPage.scss";
+
+export function MoodPage() {
+    return (
+        <main>
+            Mood page placeholder
+        </main>
+    )
+}
