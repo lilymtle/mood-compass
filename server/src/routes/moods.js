fe20a720bfb87033c5f2a2e97c676cb69da40136
Lily Le <lmlt8180@gmail.com>
2024-09-15 10:11:08 -0400

    update moods route

---
+++ server/src/routes/moods.js
@@ -0,0 +1,12 @@
+// import dependency
+import express from "express";
+
+// import controllers
+import { getMoods, getMoodById } from "../controllers/moodsControllers.js";
+
+const router = express.Router();
+
+router.get("/", getMoods);
+router.get("/:id", getMoodById);
+
+export default router;
