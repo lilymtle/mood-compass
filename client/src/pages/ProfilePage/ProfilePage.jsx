b1a5127ab3c484387ee1903a5b02de923f9b3271
Lily Le <lmlt8180@gmail.com>
2024-09-14 20:51:57 -0400

    create profile page, placeholder, and logout button feature

--- client/src/pages/ProfilePage/ProfilePage.jsx
+++ client/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,9 +1,26 @@
+import { Button } from "../../components/Button/Button";
 import "./ProfilePage.scss";
 
+import { logoutUser } from "../../auth/authUtils";
+import { navigationHandler } from "../../utils/navigationHandler";
+
 export function ProfilePage() {
+    const navigateTo = navigationHandler();
+
+    const handleLogout = () => {
+        logoutUser();
+        navigateTo("/");
+    }
+
     return (
-        <>
-            Profile placeholder
-        </>
+        <main>
+            Placeholder for profile
+
+            <Button
+            className="logout-btn"
+            type="submit"
+            text="Logout"
+            onClick={handleLogout} />
+        </main>
     )
 };
