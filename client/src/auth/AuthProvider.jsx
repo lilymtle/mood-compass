a8a7fba7db9bf0aa612629acdbc94586ace068c8
Lily Le <lmlt8180@gmail.com>
2024-09-14 20:50:07 -0400

    create authprovider and context

---
+++ client/src/auth/AuthProvider.jsx
@@ -0,0 +1,26 @@
+import { createContext, useEffect, useState } from "react";
+
+// import authentication
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebaseAuth.js";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user, loading}}>
+            {loading ? <div>Loading...</div> : children }
+        </AuthContext.Provider>
+    );
+};
