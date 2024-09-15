0c568a1587c6d2852b14ab1375c302a227f7b47c
Lily Le <lmlt8180@gmail.com>
2024-09-15 11:06:16 -0400

    import hooks, ev, and create page structure

--- client/src/pages/MoodsPage/MoodPage/MoodPage.jsx
+++ client/src/pages/MoodsPage/MoodPage/MoodPage.jsx
@@ -1,12 +1,24 @@
 /* this page is for a single mood and provides more 
 detailed information on it */
 
+// import styling
 import "./MoodPage.scss";
+
+// import environment variable
+const baseURL = import.meta.env.VITE_API_URL;
+
+// import hooks
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export function MoodPage() {
     return (
         <main>
-            Mood page placeholder
+            <section className="mood">
+                <h1 className="mood__header">
+                    {selectedMood.title}
+                </h1>
+            </section>
         </main>
-    )
-}
+    );
+};
