7fc553122966ed217ef8135e1293dbbf28778a6e
Lily Le <lmlt8180@gmail.com>
2024-09-14 21:54:55 -0400

    update knexfile

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,47 +1,10 @@
-// Update with your config settings.
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
-module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+export default {
+  client: "mysql2",
+  connection: {
+    host: "127.0.0.1",
+    user: "root",
+    password: "rootroot",
+    database: "mood_compass",
+    charset: "utf8"
   }
-
 };
