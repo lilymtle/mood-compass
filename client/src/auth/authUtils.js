d4e88349adef32df0c9a1d1617db021aedb7a0dd
Lily Le <lmlt8180@gmail.com>
2024-09-14 21:16:03 -0400

    add post request and payload to handleRegister

--- client/src/auth/authUtils.js
+++ client/src/auth/authUtils.js
@@ -2,10 +2,16 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } f
 import { auth } from "./firebaseAuth.js";
 
 // register a new user
-export const registerUser = async (email, password) => {
+export const registerUser = async (email, password, name) => {
     try {
         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        return userCredential.user;
+        const user = userCredential.user;
+
+        await axios.post("/api/users", {
+            uid: user.uid,
+            name: name,
+            email: email
+        });
     } catch (error) {
         console.error("Unable to register:", error);
     };
