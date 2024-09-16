87219a884002218ec757b28167d3b1a811ee5867
Lily Le <lmlt8180@gmail.com>
2024-09-16 11:12:11 -0400

    create educational resources routes

---
+++ server/src/routes/educationalResources.js
@@ -0,0 +1,12 @@
+// import dependency
+import express from "express";
+
+// import controllers
+import { getResourceById, getResources } from "../controllers/educationalResourcesControllers.js";
+
+const router = express.Router();
+
+router.get("/", getResources);
+router.get("/:id", getResourceById);
+
+export default router;
