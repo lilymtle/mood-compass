760196eed5c65913b27831f053d506d9ff330549
Lily Le <lmlt8180@gmail.com>
2024-09-16 15:30:39 -0400

    create and update coping strategies routes

---
+++ server/src/routes/copingStrategies.js
@@ -0,0 +1,12 @@
+// import dependency
+import express from "express";
+
+// import controllers
+import { getStrategies, getStrategyById } from "../controllers/copingStrategiesControllers.js";
+
+const router = express.Router();
+
+router.get("/", getStrategies);
+router.get("/:id", getStrategyById);
+
+export default router;
