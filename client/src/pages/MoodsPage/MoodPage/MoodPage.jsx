f2613d393a805c9d64e9d52d53ea5fa4f2180dbf
Lily Le <lmlt8180@gmail.com>
2024-09-15 21:45:11 -0400

    import ev, axios, and fetch mood data

--- client/src/pages/MoodsPage/MoodPage/MoodPage.jsx
+++ client/src/pages/MoodsPage/MoodPage/MoodPage.jsx
@@ -4,19 +4,39 @@ detailed information on it */
 // import styling
 import "./MoodPage.scss";
 
-// import environment variable
-const baseURL = import.meta.env.VITE_API_URL;
+// import dependency
+import axios from "axios";
 
 // import hooks
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+// import environmental variable
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
 export function MoodPage() {
+    const { id } = useParams();
+    const [mood, setMood] = useState("");
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const getMood = async () => {
+            try {
+                const { data } = await axios.get(`${baseURL}/api/moods/${id}`)
+                setMood(data);
+            } catch (error) {
+                console.error("Error fetching mood data:", error);
+                setError("Failed to fetch mood data.");
+            };
+        };
+        getMood();
+    }, [id])
+
     return (
         <main>
             <section className="mood">
                 <h1 className="mood__header">
-                    {selectedMood.title}
+                    {mood.name}
                 </h1>
             </section>
         </main>
