fb4914d0b0024daabbb96aedfee0d7b049bd02d7
Lily Le <lmlt8180@gmail.com>
2024-09-17 09:37:16 -0400

    create user services to handle user data

---
+++ server/src/services/userServices.js
@@ -0,0 +1,18 @@
+import { db } from "../config/db.js";
+
+export async function handleUserData(firebaseUid, name, email) {
+    const existingUser = 
+    await db("users").where({ firebase_uid: firebaseUid }).first();
+    console.log("Existing user:", existingUser);
+
+    if (existingUser) {
+        await db("users").where({ firebase_uid: firebaseUid }).update( {name, email });
+    } else {
+        await db("users").insert({
+            firebase_uid: firebaseUid,
+            name,
+            email
+        });
+        console.log("User inserted")
+    };
+};
