7f806e88786e8e95cced1c1e98d58f081d1769ed
Lily Le <lmlt8180@gmail.com>
2024-09-17 09:34:30 -0400

    update users controller
    - add register and login functions

--- server/src/controllers/usersControllers.js
+++ server/src/controllers/usersControllers.js
@@ -1,3 +1,47 @@
+import admin from "../config/firebaseAdmin.js";
+import { handleUserData } from "../services/userServices.js";
+import { db } from "../config/db.js";
+
 // import environment variables
 const { PORT } = process.env;
 const port = PORT || 8080;
+
+export const registerUser = async (req, res) => {
+    console.log("Register request body:", req.body);
+    const { idToken, name, email } = req.body;
+
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(idToken);
+        const firebaseUid = decodedToken.uid;
+
+        await handleUserData(firebaseUid, name, email);
+
+        res.status(200).send("User registered successfully.");
+    } catch (error) {
+        console.error("Error registering user:", error);
+        res.status(500).send("Error registering user.");
+    };
+};
+
+export const loginUser = async (req, res) => {
+    const { idToken } = req.body;
+
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(idToken);
+        const firebaseUid = decodedToken.uid;
+
+        const user = 
+        await db("users")
+        .where( {firebase_uid: firebaseUid })
+        .first();
+
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).send("User not found.");
+        }
+    } catch (error) {
+        console.error("Error logging in user:", error);
+        res.status(500).send("Error logging in user.");
+    };
+};
