bb4f2d71b09a4154518d520f19a005aacec2a3b2
Lily Le <lmlt8180@gmail.com>
2024-09-17 16:33:41 -0400

    update mood controllers to fetch data from db instead of json

--- server/src/controllers/moodsControllers.js
+++ server/src/controllers/moodsControllers.js
@@ -1,30 +1,39 @@
-// import core modules
-import path from "path";
-import { readFileSync } from "fs";
-import { fileURLToPath } from "url";
+import { db } from "../config/db.js";
 
-// retrieve JSON file
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const jsonFilePath = path.join(__dirname, "..", "data", "moods.json");
+// // import core modules
+// import path from "path";
+// import { readFileSync } from "fs";
+// import { fileURLToPath } from "url";
 
-const moods = JSON.parse(readFileSync(jsonFilePath, "utf-8"));
+// // retrieve JSON file
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const jsonFilePath = path.join(__dirname, "..", "data", "moods.json");
 
-export const getMoods = (req, res) => {
-    if (moods) {
+// const moods = JSON.parse(readFileSync(jsonFilePath, "utf-8"));
+
+export const getMoods = async (req, res) => {
+    try {
+        const moods = await db("moods").select("*");
         res.status(200).json(moods);
-    } else {
+    } catch (error) {
+        console.error("Error retrieving moods:", error);
         res.status(500).json({message: "Unable to retrieve moods data at this time."})
     }
 };
 
-export const getMoodById = (req, res) => {
+export const getMoodById = async (req, res) => {
     const { id } = req.params;
-    const mood = moods.find(mood => mood.id === id);
 
-    if (mood) {
-        res.status(200).json(mood);
-    } else {
-        res.status(404).json({message: "No mood with that id was found."});
-    };
+    try {
+        const mood = await db("moods").where({ id }).first();
+        if (mood) {
+            res.status(200).json(mood);
+        } else {
+            res.status(404).json({ message: "No mood with that id was found." });
+        }
+    } catch (error) {
+        console.error(`Error fetching mood with ID ${id}:`, error);
+        res.status(500).json({ message: "Unable to retrieve mood data at this time."})
+    }
 };
