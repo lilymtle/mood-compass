2377f0a832e56dd74984dfa4c671518de6b8f818
Lily Le <lmlt8180@gmail.com>
2024-09-18 09:24:32 -0400

    update educational resources migration file

--- server/migrations/20240918130955_create_educational_resources.js
+++ server/migrations/20240918130955_create_educational_resources.js
@@ -1,15 +1,15 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+export function up(knex) {
+    return knex.schema.createTable("educational_resources", function(table) {
+        table.increments("id").primary();
+        table.string("name").notNullable();
+        table.text("short_description").notNullable();
+        table.json("descriptions").notNullable();
+        table.json("images").notNullable();
+        table.json("strategies").notNullable();
+        table.json("resources").notNullable();
+    })
+}
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+export function down(knex) {
+    return knex.schema.dropTableIfExists("educational_resources");
+}
