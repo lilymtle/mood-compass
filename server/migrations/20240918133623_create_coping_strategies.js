9fb9a7e80f5d2e41103f54eaaf91bd92059f0c4f
Lily Le <lmlt8180@gmail.com>
2024-09-18 09:50:58 -0400

    update coping strategies migration file

--- server/migrations/20240918133623_create_coping_strategies.js
+++ server/migrations/20240918133623_create_coping_strategies.js
@@ -1,15 +1,16 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+export function up(knex) {
+    return knex.schema.createTable("coping_strategies", function(table) {
+        table.increments("id").primary();
+        table.string("name").notNullable();
+        table.text("short_description").notNullable();
+        table.json("descriptions").notNullable();
+        table.json("images").notNullable();
+        table.json("steps").notNullable();
+        table.json("benefits").notNullable();
+        table.json("resources").notNullable();
+    })
+}
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+export function down(knex) {
+    return knex.schema.dropTableIfExists("coping_strategies");
+}
