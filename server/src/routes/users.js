1681aeefd87360eb3b1c2cbb1fd45b3bfc53cc37
Lily Le <lmlt8180@gmail.com>
2024-09-19 09:28:50 -0400

    update users route

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -2,11 +2,12 @@
 import express from "express";
 
 // import controllers
-import { loginUser, registerUser } from "../controllers/usersControllers.js";
+import { getUserName, loginUser, registerUser } from "../controllers/usersControllers.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/name", getUserName);
 
 export default router;
