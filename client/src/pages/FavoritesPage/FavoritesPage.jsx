f294c7e45b38c7aeb202499961ea14a1b4c16c53
Lily Le <lmlt8180@gmail.com>
2024-09-20 01:03:16 -0400

    delete unused code

--- client/src/pages/FavoritesPage/FavoritesPage.jsx
+++ client/src/pages/FavoritesPage/FavoritesPage.jsx
@@ -96,11 +96,6 @@ export function FavoritesPage() {
         <main>
             <section className="favorites">
                 <div className="favorites__container">
-                <div className="favorites__divider-curve">
-                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                        <path d="M0,0V7.23C0,65.52,268.63,112.77,600,112.77S1200,65.52,1200,7.23V0Z" className="shape-fill"></path>
-                    </svg>
-                </div>
                     <h1 className="favorites__header">
                         Your Favorites
                     </h1>
