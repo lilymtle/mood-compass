a2b64acb4737c3db61312c77ffd4ec5d44080107
Lily Le <lmlt8180@gmail.com>
2024-09-20 02:17:14 -0400

    update class names and add values img

--- client/src/pages/AboutPage/AboutPage.jsx
+++ client/src/pages/AboutPage/AboutPage.jsx
@@ -1,3 +1,4 @@
+//import styling
 import "./AboutPage.scss";
 
 export function AboutPage() {
@@ -13,7 +14,7 @@ export function AboutPage() {
                         <h2 className="about__subheader">
                             What is MoodCompass?
                         </h2>
-                        <p className="about__text">
+                        <p className="about__text--white">
                             MoodCompass is a passion project of mine. It is a capstone project for my
                             Software Engineering program at BrainStation. I am passionate about mental
                             health, and that played a huge role in deciding what my capstone project
@@ -22,16 +23,16 @@ export function AboutPage() {
                     </div>
 
                     <div className="about__content-container--box">
-                        <h2 className="about__subheader">
+                        <h2 className="about__subheader--box">
                                 Why MoodCompass?
                         </h2>
-                        <p className="about__text">
+                        <p className="about__text--grey">
                             I worked as a registered nurse for over 5 years and I specialized in
                             mental health and addictions. As someone who worked with individuals
                             who experienced mental health issues, and as someone with lived 
                             experience, advocating for mental health is close to my heart. 
                         </p>
-                        <p className="about__text">
+                        <p className="about__text--grey">
                             One of my goals with MoodCompass is to create a safe space where
                             individuals can navigate through difficult emotions. I strongly
                             believe that knowledge is a powerful tool that can change lives.
@@ -42,6 +43,10 @@ export function AboutPage() {
 
             <section className="about__values">
                 <div className="about__values-container">
+                    <img
+                        className="about__values-img"
+                        src="/src/assets/images/mood-compass-values.png"
+                        alt="moodcompass values" />
                 </div>
             </section>
         </main>
