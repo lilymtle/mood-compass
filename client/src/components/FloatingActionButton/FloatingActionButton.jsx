e0caec77aa2c3ead73d68519d5b9512f7f23985a
Lily Le <lmlt8180@gmail.com>
2024-09-21 13:40:11 -0400

    create floating action button
    - update styling
    - add scroll function so that it renders when a user scrolls away from the top of the browser

---
+++ client/src/components/FloatingActionButton/FloatingActionButton.jsx
@@ -0,0 +1,72 @@
+// import styling
+import "./FloatingActionButton.scss";
+
+// import hooks
+import { useEffect, useState } from "react";
+
+// import components
+import Box from '@mui/material/Box';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import HomeIcon from '@mui/icons-material/Home';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import BookIcon from '@mui/icons-material/Book';
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+
+export function FloatingActionButton() {
+    const actions = [
+        { icon: <HomeIcon />, name: "Home" },
+        { icon: <AccountCircleIcon />, name: "Profile" },
+        { icon: <EmojiEmotionsIcon />, name: "Moods" },
+        { icon: <BookIcon/>, name: "Educational Resources" },
+        { icon: <LightbulbIcon />, name: "Coping Strategies" }
+    ];
+
+    const [showSpeedDial, setSpeedDial] = useState(false);
+    const scrollThreshold = 200;
+
+    const handleScroll = () => {
+        if (window.scrollY > scrollThreshold) {
+            setSpeedDial(true);
+        } else {
+            setSpeedDial(false);
+        };
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        };
+    }, []);
+    
+    return (
+        <Box sx={{ 
+            transform: 'translateZ(0px)', 
+            flexGrow: 1,
+            position: "fixed", 
+            bottom: 16, 
+            right: 16, 
+            zIndex: 1000,
+            display: showSpeedDial ? "block" : "none"
+        }}>
+        <SpeedDial
+            ariaLabel="Floating action button with navigation that will render when a user scrolls away from the navigation bar"
+            sx={{ 
+                transform: "scale(0.8)",
+            }}
+            icon={<SpeedDialIcon />}
+        >
+            {actions.map((action) => (
+            <SpeedDialAction
+                key={action.name}
+                icon={action.icon}
+                tooltipTitle={action.name}
+            />
+            ))}
+        </SpeedDial>
+        </Box>
+    )
+};
