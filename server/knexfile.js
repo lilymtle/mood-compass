66db353d7617502c879c21a4b9d2ebea86ff3562
Lily Le <lmlt8180@gmail.com>
2024-09-22 20:41:13 -0400

    add environmental variables

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,10 +1,12 @@
+import "dotenv/config";
+
 export default {
   client: "mysql2",
   connection: {
-    host: "127.0.0.1",
-    user: "root",
-    password: "rootroot",
-    database: "mood_compass",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     charset: "utf8"
   },
   migrations: {
