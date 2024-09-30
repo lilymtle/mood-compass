6e4b4df6dac6b2e416c720ce75200206e6862a79
Lily Le <lmlt8180@gmail.com>
2024-09-30 14:03:25 -0400

    update knexfile

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,10 +3,10 @@ import "dotenv/config";
 export default {
   client: "mysql2",
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
     charset: "utf8"
   },
   migrations: {
