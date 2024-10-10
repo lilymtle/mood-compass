97739924ea47965059e5595f43c98b9395128c07
Lily Le <lmlt8180@gmail.com>
2024-10-10 13:18:02 -0400

    update charset to support emojis

--- server/knexfile.js
+++ server/knexfile.js
@@ -12,7 +12,7 @@ export default {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    charset: "utf8"
+    charset: "utf8mb4"
   },
   migrations: {
     directory: "./migrations",
