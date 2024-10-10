0ba4d1d9bb669244e4c1c9fb20904ff9141ed904
Lily Le <lmlt8180@gmail.com>
2024-10-10 13:38:05 -0400

    update mood tracker controller with boilerplate code

---
+++ server/src/controllers/moodTrackerController.js
@@ -0,0 +1,37 @@
+import { db } from "../config/db";
+
+// adds user mood tracker data
+export const addUserMood = (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+// gets user mood tracker data
+export const getUserMood = (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+// updates user mood tracker data
+export const editUserMood = (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+// deletes user mood tracker data
+export const deleteUserMood = (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
