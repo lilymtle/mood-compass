19b99ac008aa44534c9a47effec67a46db88f2dc
Lily Le <lmlt8180@gmail.com>
2024-10-10 11:48:19 -0400

    update evs for local development

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,10 +3,15 @@ import "dotenv/config";
 export default {
   client: "mysql2",
   connection: {
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
+    // host: process.env.MYSQLHOST,
+    // user: process.env.MYSQLUSER,
+    // password: process.env.MYSQLPASSWORD,
+    // database: process.env.MYSQLDATABASE,
+    // charset: "utf8"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     charset: "utf8"
   },
   migrations: {
